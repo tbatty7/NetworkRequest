b5d39f0d94bb0cf1054441c73662dbf4b4546deb
Tim Batty <tbatty7@gmail.com>
2022-02-08 17:06:10 -0500

    added button that makes network call to itunes

--- NetworkRequest/ViewController.swift
+++ NetworkRequest/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private(set) var button: UIButton!
+    
+    private var dataTask: URLSessionDataTask?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction private func buttonTapped() {
+        searchForBook(terms: "out from boneville")
+    }
 
+    private func searchForBook(terms: String) {
+        guard let encodedTerms = terms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed), let url = URL(string: "https://itunes.apple.com/search?" + "mediaType=book&term=\(encodedTerms)") else { return }
+        let request = URLRequest(url: url)
+        dataTask = URLSession.shared.dataTask(with: request) {
+            [weak self] (data: Data?, response: URLResponse?, error: Error?) -> Void in
+            guard let self = self else { return }
+            
+            let decoded = String(data: data ?? Data(), encoding: .utf8)
+            print("response: \(String(describing: response))")
+            print("data: \(String(describing: decoded))")
+            print("error: \(String(describing: error))")
+            
+            DispatchQueue.main.async {
+                [weak self] in guard let self = self else { return }
+                self.dataTask = nil
+                self.button.isEnabled = true
+            }
+        }
+        button.isEnabled = false
+        dataTask?.resume()
+    }
 }
 
