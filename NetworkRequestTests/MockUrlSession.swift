72638d7682bede2e05f59f6ad0b573907837cd5b
Mark <mark@industriallogic.com>
2022-02-09 14:57:56 -0500

    Improved error messages for DataTask

--- NetworkRequestTests/MockUrlSession.swift
+++ NetworkRequestTests/MockUrlSession.swift
@@ -24,14 +24,34 @@ class MockUrlSession: URLSessionProtocol {
     }
     
     func verifyDataTask(with request: URLRequest, file: StaticString = #file, line: UInt = #line) {
-        XCTAssertEqual(dataTaskCallCount, 1, "call count", file: file, line: line)
+        guard dataTaskWasCalledOnce(file: file, line: line) else  {
+            return
+        }
+        
         XCTAssertEqual(dataTaskArgsRequest.first, request, "request", file: file, line: line)
     }
     
+    private func dataTaskWasCalledOnce(file: StaticString = #file, line: UInt = #line) -> Bool {
+        verifyMethodCallOnce(methodName: "dataTask(with:completionHandler:)", callCount: dataTaskCallCount, describeArgument: "request: \(dataTaskArgsRequest)", namedFile: file, line: line)
+    }
 }
 
 private class DummyUrlSessionDataTask: URLSessionDataTask {
     override func resume() {
         // do nothing
     }
+}
+
+func verifyMethodCallOnce(methodName: String, callCount: Int, describeArgument: @autoclosure () -> String, namedFile: StaticString = #file, line: UInt = #line) -> Bool {
+    if callCount == 0 {
+        XCTFail("Wanted but not invoked \(methodName)", file: namedFile, line: line)
+        return false
+    }
+    
+    if callCount > 1 {
+        XCTFail("Wanted 1 time but was called \(callCount) times. \(methodName) with \(describeArgument())", file: namedFile, line: line)
+        return false
+    }
+    
+    return true
 }
