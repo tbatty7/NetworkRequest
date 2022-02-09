6130eb62d00903fc6f8c5ee52a1a019d91569baf
Tim Batty <tbatty7@gmail.com>
2022-02-09 10:20:43 -0500

    Created test spy for URLSession

---
+++ NetworkRequestTests/MockUrlSession.swift
@@ -0,0 +1,25 @@
+//
+//  MockUrlSession.swift
+//  NetworkRequestTests
+//
+//  Created by Timothy D Batty on 2/9/22.
+//
+
+import Foundation
+@testable import NetworkRequest
+
+class MockUrlSession: URLSessionProtocol {
+    
+    var dataTaskCallCount = 0
+    var dataTaskArgsRequest: [URLRequest] = []
+    
+    func dataTask(
+        with request: URLRequest,
+        completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void
+    ) -> URLSessionDataTask {
+        dataTaskCallCount += 1
+        dataTaskArgsRequest.append(request)
+        return URLSessionDataTask()
+    }
+    
+}
