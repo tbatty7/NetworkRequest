1cfe14d9ece2b50aeb7904cc0a29b6dea43523a1
Tim Batty <tbatty7@gmail.com>
2022-02-10 11:37:44 -0500

    Added SpyUrlSession Spy object that duplicates MockUrlSession but will test the response

---
+++ NetworkRequestTests/SpyUrlSession.swift
@@ -0,0 +1,29 @@
+//
+//  SpyUrlSession.swift
+//  NetworkRequestTests
+//
+//  Created by Timothy D Batty on 2/10/22.
+//
+
+@testable import NetworkRequest
+import Foundation
+
+private class DummyUrlSessionDataTask: URLSessionDataTask {
+    override func resume() {
+        
+    }
+}
+
+class SpyUrlSession: URLSessionProtocol {
+    
+    var dataTaskCallCount = 0
+    var dataTaskArgsRequest: [URLRequest] = []
+    var dataTaskArgsCompletionHandler: [(Data?, URLResponse?, Error?) -> Void] = []
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        dataTaskCallCount += 1
+        dataTaskArgsRequest.append(request)
+        dataTaskArgsCompletionHandler.append(completionHandler)
+        return DummyUrlSessionDataTask()
+    }
+}
